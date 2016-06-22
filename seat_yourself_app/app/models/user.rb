de9906e1adc5854d3fcef5a4e215c0ef9025ebd0
spencer <spencer.dossett@gmail.com>
2016-06-22 17:18:19 -0400

    corrected broken assocation for restaurants <=> owners

--- seat_yourself_app/app/models/user.rb
+++ seat_yourself_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :reservations
   has_many :restaurants, through: :reservations
-  has_many :owned_restaurants, class_name: 'restaurant'
+  has_many :owned_restaurants, class_name: 'Restaurant', foreign_key: 'user_id'
 end
