83776489f1d97c40e6255e6b4d0d71e180bb99e2
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-06-23 16:03:30 -0400

    added restaurant validation and session functionality

---
+++ seat_yourself_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to restaurants_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render "new"
+    end
+
+
+    # if user && user.authenticate(params[:password])
+    #   session[:user_id] = user.id
+    #   redirect_to products_url, notice: "Logged in!"
+    # else
+    #   flash.now[:alert] = "Invalid email or password"
+    #   render "new"
+    # end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_url, notice: "Logged out!"
+  end
+end
