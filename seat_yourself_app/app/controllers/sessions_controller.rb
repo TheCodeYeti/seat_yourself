9c2ec40cdfcfbbeec92a53f5486f09d40ba8c1a6
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-06-27 15:24:16 -0400

    Eliminated the logged in and out messages.

--- seat_yourself_app/app/controllers/sessions_controller.rb
+++ seat_yourself_app/app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
 
     if user
       session[:user_id] = user.id
-      redirect_to restaurants_owner_list_path, notice: "Logged in!"
+      redirect_to restaurants_owner_list_path
+      # , notice: "Logged in!"
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
@@ -27,6 +28,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to restaurants_url, notice: "Logged out!"
+    redirect_to restaurants_url
+    # , notice: "Logged out!"
   end
 end
